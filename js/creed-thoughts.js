765cfd6f54b7b82baf1cfdad3c816946331f5a67
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2022-03-24 09:27:48 -0400

    font awesome kit

---
+++ js/creed-thoughts.js
@@ -0,0 +1,76 @@
+const creedThoughts = [
+  ,
+  `Just pretend like we’re talking until the cops leave.`,
+  `I’ve been involved in a number of cults, both as a leader and a follower. You have more fun as a follower, but you make more money as a leader.`,
+  `I’ve never owned a refrigerator before.`,
+  `Cool beans, man! I live by the quarry. We should hang out by the quarry and throw things down there.`,
+  `Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name is Creed Bratton.`,
+  `The Taliban’s the worst. Great Heroin, though.`,
+  `You ever see a guy with four toes?`,
+  `A lot of jazz cats are blind. But they can play the piano like nobody’s business. I’d like to put the piano in front of Pam, without her glasses, and see what happens. I’d also like to see her topless.`,
+  `Creed Bratton has never declared bankruptcy. When Creed Bratton gets into trouble, he transfers his debt to William Charles Schneider.`,
+  `I’m a pretty normal guy. I do one weird thing. I like to go in the women’s room for number two. I’ve been caught several times, and I have paid dearly.`,
+  `When Pam gets Michael’s old chair, I get Pam’s old chair. Then I’ll have two chairs. Only one more to go.`,
+  `The only difference between me and a homeless man is this job. I will do whatever it takes to survive, just like I did when I was a homeless man.`,
+  `I run a small fake I.D. company from my car with a laminating machine that I swiped from the sheriff’s station.`,
+  `Have you ever noticed you can only ooze two things? Sexuality and pus. Man, I tell ya.`,
+  `Did one of you tell Stanley that I have asthma? Cause I don’t. If this gets out, they won’t let me scuba. And if I can’t scuba, what am I working towards?`,
+  `A beautiful morning at Dunder Mifflin, or like I like to call it, Great Bratton.`,
+  `Guys, I’m starting to think Pam’s not even pregnant.`,
+  `I am not offended by homosexuality. In the sixties, I made love to many, many, many women, often outdoors in the mud and the rain. It’s possible a man could’ve slipped in there. There would be no way of knowing.`,
+  `He’s been trashing us relentlessly on Twitter. Now it’s funny stuff, but mean.`,
+  `I already won the lottery. I was born in the US of A baby! And as a backup, I have a Swiss passport.`,
+  `You know a human can go on living for several hours after being decapitated.`,
+  `Hey, brah. I have been meaning to ask you. Can we get some Red Bull for these things? Sometimes your guy’s gotta rude the bull. Later skater.`,
+  `It’s Halloween? That is really, really good timing.`,
+  `If my parents see this, I’m toast.`,
+  `That is Northern Lights Cannabis Indica,`,
+  `Darnell’s a chump. Andrea’s the office bitch; you’ll get used to her. Which one is Pam? Have I ever steered you wrong Jim? (to Andy). Goodnight, Mary-Beth.`,
+  `You deal with this, or you, me Sammy, Phyllis, the chick you hit with the car…we’re goners.`,
+  `I sprout mung beans on a damp paper towel in my desk drawer. Very nutritious, but they smell like death.`,
+  `Jinx, buy me some coke.`,
+  `A wheel wants to spin, Pam.`,
+  `It’s Creed. FYI I’m starting my own paper company looking to poach some chumps. You in?
+, Let’s keep this on the QT, okay? I want to be a dead mama JAMA.`,
+  `Two eyes, two ears, a chin, a mouth, 10 fingers, two nipples, a butt, two kneecaps, a penis. I have just described to you the Lochness Monster and the reward for its capture…all the riches in Scotland. So I have one question, why are you her?`,
+  `My tombstone has been already made, thank you.`,
+  `I vant to sell your blood.`,
+  `Be cool, Michael. I saw this guy kill a bunch of people.`,
+  `All right, all right. Say no more. So, this is how I got Squeaky Fromme. No small talk. Just show her who’s the boss. Just go right in and kiss her.`,
+  `What’s a text?`,
+  `The ball’s on you, man.`,
+  `It’s pronounced Ker-nell. It’s the highest rank in the army.`,
+  `I can bring these to my shelter.`,
+  `What is wrong with this woman? She’s asking about stuff that’s nobody’s business. What do I do? Really, what do I do here? I should’ve written it down. Qua something, uh…qua…quar…quibo, qual…quir-qubity. Uabity assurance! No, no, no, no, no, but I’m getting close.`,
+  `You’re over 40; that’s the cut-off. Are you listening to what he’s saying? Re-training. New system. Youth. I’m telling you, this kid is the grim reaper.`,
+  `Was any of this real?`,
+];
+
+const getCreedThought = () => {
+  return creedThoughts[Math.floor(Math.random() * creedThoughts.length)];
+};
+
+module.exports = getCreedThought;
+// Related: Robert California Quotes
+// , `Oh really, what kind? Codeine, Vicodin, Percoset, Fentanyl, Oxycontin, Palladone? What….`
+// , `yes. Hello, Creed Bratton, Quality Assurance, Dunder Mifflin, Scranton. I was supposed to meet with one of your floor managers last week for a quality inspection, and he or she wasn’t there. I’m trying to remember who it was. Who wasn’t there last week?`
+// ,` Every week, I’m supposed to take four hours and do a quality spot-check at the paper mill. And of course, the one year I blow it off, this happens.`
+// , `Animals can’t feel pain.`
+// , `He was drunk as a skunk. He was flying down Route 6. He slides under an 18-wheeler. Pop. Snaps right off.`
+// , `I was in an iron lung when I was a teenager.`
+// , `Not bad for a day in the life of a dog food company.`
+// , `You should see how many supplies I’ve taken from this place. Honestly, I love stealing.`
+// , `Everywhere I look, it’s Betty White this and Betty White that. Finally, a kid who is not talking about Betty White. Of course, I follow him.`, `You’re paying way too much for your worms, man. Who’s your worm guy?`
+// , `That wasn’t a tape worm` (Talking about the worm he sold Kelly to eat so she could lose weight.)
+// , `I don’t want to take myself too seriously. That’s my lesson to myself.`
+// , `Ohm, I steal things all the time. It’s just something I do. I stopped caring a long time ago.`
+// , `In case you were wondering, my spirit animal is a duck-billed platypus.`
+// , `Apparently, I don’t want to take myself too seriously.`
+// , `There’s not that many people from the sixties who have progressed as writers and are continuing on. They’re out there. But I’m one of them who’s just continued on, following his own little inner madness.`
+// , `I was with a folk trio back in ’63 and ’64, and we traveled all across North Africa, Israel, and Europe.`
+// , `I was a drama major through college.`
+// , `I don’t sit down to write a song, per se.`, `I’m a survivalist and a survivor.`
+// , `I wanna do a cartwheel. But real casual like, not make a bit deal out of it, but I know everyone saw it. Just one stunning gorgeous cartwheel.`
+// , `It’s pirate code. It means he wants to meet. I understand pirate code. I can’t speak it.`
+// , `Get your eyes checked, chucklehead.`
+//
--- js/game.js
+++ js/game.js
@@ -6,6 +6,58 @@ const resultsDivContainer = document.querySelector(".results-container");
 const inputs = document.querySelectorAll("input");
 const buttons = document.querySelectorAll("button");
 
+const creedThoughts = [
+  ,
+  `Just pretend like we’re talking until the cops leave.`,
+  `I’ve been involved in a number of cults, both as a leader and a follower. You have more fun as a follower, but you make more money as a leader.`,
+  `I’ve never owned a refrigerator before.`,
+  `Cool beans, man! I live by the quarry. We should hang out by the quarry and throw things down there.`,
+  `Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name is Creed Bratton.`,
+  `The Taliban’s the worst. Great Heroin, though.`,
+  `You ever see a guy with four toes?`,
+  `A lot of jazz cats are blind. But they can play the piano like nobody’s business. I’d like to put the piano in front of Pam, without her glasses, and see what happens. I’d also like to see her topless.`,
+  `Creed Bratton has never declared bankruptcy. When Creed Bratton gets into trouble, he transfers his debt to William Charles Schneider.`,
+  `I’m a pretty normal guy. I do one weird thing. I like to go in the women’s room for number two. I’ve been caught several times, and I have paid dearly.`,
+  `When Pam gets Michael’s old chair, I get Pam’s old chair. Then I’ll have two chairs. Only one more to go.`,
+  `The only difference between me and a homeless man is this job. I will do whatever it takes to survive, just like I did when I was a homeless man.`,
+  `I run a small fake I.D. company from my car with a laminating machine that I swiped from the sheriff’s station.`,
+  `Have you ever noticed you can only ooze two things? Sexuality and pus. Man, I tell ya.`,
+  `Did one of you tell Stanley that I have asthma? Cause I don’t. If this gets out, they won’t let me scuba. And if I can’t scuba, what am I working towards?`,
+  `A beautiful morning at Dunder Mifflin, or like I like to call it, Great Bratton.`,
+  `Guys, I’m starting to think Pam’s not even pregnant.`,
+  `I am not offended by homosexuality. In the sixties, I made love to many, many, many women, often outdoors in the mud and the rain. It’s possible a man could’ve slipped in there. There would be no way of knowing.`,
+  `He’s been trashing us relentlessly on Twitter. Now it’s funny stuff, but mean.`,
+  `I already won the lottery. I was born in the US of A baby! And as a backup, I have a Swiss passport.`,
+  `You know a human can go on living for several hours after being decapitated.`,
+  `Hey, brah. I have been meaning to ask you. Can we get some Red Bull for these things? Sometimes your guy’s gotta rude the bull. Later skater.`,
+  `It’s Halloween? That is really, really good timing.`,
+  `If my parents see this, I’m toast.`,
+  `That is Northern Lights Cannabis Indica,`,
+  `Darnell’s a chump. Andrea’s the office bitch; you’ll get used to her. Which one is Pam? Have I ever steered you wrong Jim? (to Andy). Goodnight, Mary-Beth.`,
+  `You deal with this, or you, me Sammy, Phyllis, the chick you hit with the car…we’re goners.`,
+  `I sprout mung beans on a damp paper towel in my desk drawer. Very nutritious, but they smell like death.`,
+  `Jinx, buy me some coke.`,
+  `A wheel wants to spin, Pam.`,
+  `It’s Creed. FYI I’m starting my own paper company looking to poach some chumps. You in?
+, Let’s keep this on the QT, okay? I want to be a dead mama JAMA.`,
+  `Two eyes, two ears, a chin, a mouth, 10 fingers, two nipples, a butt, two kneecaps, a penis. I have just described to you the Lochness Monster and the reward for its capture…all the riches in Scotland. So I have one question, why are you her?`,
+  `My tombstone has been already made, thank you.`,
+  `I vant to sell your blood.`,
+  `Be cool, Michael. I saw this guy kill a bunch of people.`,
+  `All right, all right. Say no more. So, this is how I got Squeaky Fromme. No small talk. Just show her who’s the boss. Just go right in and kiss her.`,
+  `What’s a text?`,
+  `The ball’s on you, man.`,
+  `It’s pronounced Ker-nell. It’s the highest rank in the army.`,
+  `I can bring these to my shelter.`,
+  `What is wrong with this woman? She’s asking about stuff that’s nobody’s business. What do I do? Really, what do I do here? I should’ve written it down. Qua something, uh…qua…quar…quibo, qual…quir-qubity. Uabity assurance! No, no, no, no, no, but I’m getting close.`,
+  `You’re over 40; that’s the cut-off. Are you listening to what he’s saying? Re-training. New system. Youth. I’m telling you, this kid is the grim reaper.`,
+  `Was any of this real?`,
+];
+
+const getCreedThought = () => {
+  return creedThoughts[Math.floor(Math.random() * creedThoughts.length)];
+};
+
 const finalResults = [];
 
 let wordleNumber;
@@ -37,6 +89,7 @@ const getWordle = async () => {
   )
     .then((response) => response.json())
     .then((data) => {
+      data.sort((a, b) => a.index - b.index);
       // console.log(data);
       todaysSolution = data[wordleNumber].wordle;
       // console.log(todaysSolution);
@@ -118,6 +171,14 @@ const endGame = () => {
     }
     textDiv.append(newLine);
   });
+  const creedThoughtP = document.createElement("p");
+  const creedThoughtH = document.createElement("p");
+  creedThoughtH.classList.add("creed-thought-h");
+  creedThoughtP.classList.add("creed-thought-p");
+  creedThoughtH.textContent = `Creed Thought of the day:`;
+  creedThoughtP.textContent = `"${getCreedThought()}"`;
+
+  textDiv.append(creedThoughtH, creedThoughtP);
   const closeBtn = document.createElement("p");
   closeBtn.textContent = "close";
   closeBtn.classList.add("close-results");
@@ -129,6 +190,7 @@ const endGame = () => {
     guessFormContainer.classList.remove("hide");
   });
 };
+
 const wordle = (input, solution) => {
   const guess = input.toLowerCase();
   const lineResults = [];
@@ -158,9 +220,9 @@ const wordle = (input, solution) => {
       //   howManyChecked
       // );
       // if (howManyChecked <= howManySolution) {
-      lineResults.push("🟩");
+      lineResults.push("💚");
       // } else {
-      //   lineResults.push("⬛");
+      //   lineResults.push("⚫️");
       // }
     } else if (solution.includes(guess[i])) {
       let howManySolution = 0;
@@ -185,15 +247,15 @@ const wordle = (input, solution) => {
           }
         }
         if (isThereAnotherGuessedOfTheSameLetterThatWillBeGreen) {
-          lineResults.push("⬛");
+          lineResults.push("⚫️");
         } else {
-          lineResults.push("🟨");
+          lineResults.push("🟡");
         }
       } else {
-        lineResults.push("⬛");
+        lineResults.push("⚫️");
       }
     } else {
-      lineResults.push("⬛");
+      lineResults.push("⚫️");
     }
   }
   finalResults.push(lineResults.join(""));
@@ -239,10 +301,10 @@ const submitHandler = (e) => {
             for (let i = 0; i < 5; i++) {
               const box = document.createElement("div");
               box.textContent = guessInputValue[i];
-              if (response[i] === "🟩") {
+              if (response[i] === "💚") {
                 box.classList.add("green", "box");
                 guessLabel.append(box);
-              } else if (response[i] === "🟨") {
+              } else if (response[i] === "🟡") {
                 box.classList.add("yellow", "box");
                 guessLabel.append(box);
               } else {
@@ -260,4 +322,5 @@ const submitHandler = (e) => {
     alert("please guess a 5 letter word");
   }
 };
+
 guessFormContainer.addEventListener("submit", submitHandler);
